3c96e3e7347081b3c0756e997436532688a1dfb1
Saumen95 <saumenroy323@gmail.com>
2020-10-02 20:20:49 +0600

    basic operations done

--- listbasic.py
+++ listbasic.py
@@ -1,7 +1,13 @@
+import collections
+
 shoplist = ['apple', 'banana', 'Grape', 'Orange']
-print(len(shoplist))
+print(len(shoplist)) # length of a list
 shoplist.append('Rice')
-print(shoplist)
+print(shoplist) # print list after adding
 shoplist.sort()
+word_count = collections.Counter(shoplist)
+print(word_count) # print the frequency of items in a list
+
 for item in shoplist:
-   print len(item.split())
+    print(len(item)) # print length of each items in a list
+
